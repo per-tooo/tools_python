f126243dedb77f50baf1ece2090d56d8f086c8cc
per-tooo <84619185+per-tooo@users.noreply.github.com>
2024-05-15 15:00:35 +0200

    Update argsreader.py

--- cmd/argsreader.py
+++ cmd/argsreader.py
@@ -7,8 +7,8 @@ def argumentIsset(argument:str) -> object:
   position = -1
   search = f"-{argument}" if len(argument) == 1 else f"--{argument}"
 
-  for index in range(0, len(getArgumentsList())):
-    item = getArgumentsList()[index]
+  for index in range(0, len(getArgumentList())):
+    item = getArgumentList()[index]
     if item == search:
       isset = index
 
@@ -19,7 +19,7 @@ def getArgument(argument:str, startsWithDash:bool = False) -> object:
   if position == -1:
     return None
   
-  arguments = getArgumentsList()
+  arguments = getArgumentList()
   if not((position + 1) > len(arguments)):
     value = arguments[position + 1]
     if value[0] == "-":
